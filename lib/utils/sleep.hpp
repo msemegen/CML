e7eb3779598bbb5577a31042aa0606c17e2cf308
Mateusz Semegen <mateusz.semegen@gmail.com>
2020-01-05 23:41:37 +0100

    Added sleep function

---
+++ lib/utils/sleep.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+/*
+    Name: sleep.hpp
+
+    Copyright(c) 2019 Mateusz Semegen
+    This code is licensed under MIT license (see LICENSE file for details)
+*/
+
+//cml
+#include <common/assert.hpp>
+#include <common/time_tick.hpp>
+#include <hal/systick.hpp>
+
+namespace cml {
+namespace utils {
+
+void sleep(common::time_tick a_time_ms)
+{
+    _assert(true == hal::c_systick::get_instance().is_enabled());
+
+    common::time_tick start = hal::c_systick::get_instance().get_counter();
+    while (hal::c_systick::get_instance().get_counter() - start <= a_time_ms);
+}
+
+} // namespace utils
+} // namespace cml
