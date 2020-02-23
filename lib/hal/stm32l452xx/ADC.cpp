74e675968746151fa3bfdb32a61519b3da8b6391
Mateusz Semegen <mateusz.semegen@gmail.com>
2020-02-24 00:03:21 +0100

    Started ADC impl for L4

---
+++ lib/hal/stm32l452xx/ADC.cpp
@@ -0,0 +1,21 @@
+/*
+    Name: ADC.cpp
+
+    Copyright(c) 2020 Mateusz Semegen
+    This code is licensed under MIT license (see LICENSE file for details)
+*/
+
+#ifdef STM32L452xx
+
+//this
+#include <hal/stm32l452xx/ADC.hpp>
+
+namespace cml {
+namespace hal {
+namespace stm32l452xx {
+
+} // namespace stm32l452xx
+} // namespace hal
+} // namespace cml
+
+#endif
---
+++ lib/hal/stm32l452xx/ADC.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+/*
+    Name: USART.cpp
+
+    Copyright(c) 2020 Mateusz Semegen
+    This code is licensed under MIT license (see LICENSE file for details)
+*/
+
+//externals
+#include <stm32l452xx.h>
+
+//cml
+#include <common/integer.hpp>
+
+namespace cml {
+namespace hal {
+namespace stm32l4xx {
+
+class ADC
+{
+public:
+
+    enum class Id : common::uint32
+    {
+        _1 = 0u
+    };
+
+    struct Config
+    {
+        enum class Resolution : common::uint32
+        {
+            _6_bit = ADC_CFGR_RES_1 | ADC_CFGR_RES_0,
+            _8_bit = ADC_CFGR_RES_1,
+            _10_bit = ADC_CFGR_RES_0,
+            _12_bit = 0u,
+        };
+
+    };
+
+    struct Channel
+    {
+        enum class Id : common::uint32
+        {
+            unknown
+        };
+
+        enum class Sampling_time : common::uint32
+        {
+            unknown
+        };
+
+        Id id                       = Id::unknown;
+        Sampling_time sampling_time = Sampling_time::unknown;
+    };
+
+
+public:
+
+    void enable(const Config& a_config);
+    void disable();
+
+    void set_channels(const Channel* a_p_channels, common::uint32 a_channels_count);
+    void clear_channels();
+};
+
+} // namespace stm32l4xx
+} // namespace hal
+} // namespace cml
