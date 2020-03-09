9d607a3525f9342c4125643303a61f252fb46dcb
Mateusz Semegen <mateusz.semegen@gmail.com>
2020-03-09 15:13:20 +0100

    Added ADC sample for L4

--- samples/stm32l452xx/adc/main.cpp
+++ samples/stm32l452xx/adc/main.cpp
@@ -17,61 +17,26 @@
 #include <utils/Console.hpp>
 #include <utils/sleep.hpp>
 
-volatile bool wait = true;
-volatile cml::common::uint16 d = 0;
+namespace {
 
-bool callback(cml::common::uint16 a_data, bool a_series_emd, bool a_timeout)
+using namespace cml::common;
+using namespace cml::hal;
+
+int32 compute_temperature(const ADC::Calibration_data& a_calibration_data, uint32 measure)
 {
-    wait = false;
-    d = a_data;
-    return true;
+    int32 temperature = 100 * ((measure * 330 / 300) - a_calibration_data.temperature_sensor_data_1);
+    temperature /= (a_calibration_data.temperature_sensor_data_2 - a_calibration_data.temperature_sensor_data_1);
+
+    return temperature + 30;
 }
 
-#define VREFINT_CAL_ADDR                   ((uint16_t*) (0x1FFF75AAU)) /* Internal voltage reference, address of parameter VREFINT_CAL: VrefInt ADC raw data acquired at temperature 30 DegC (tolerance: +-5 DegC), Vref+ = 3.0 V (tolerance: +-10 mV). */
-#define VREFINT_CAL_VREF                   ( 3000U)                    /* Analog voltage reference (Vref+) value with which temperature sensor has been calibrated in production (tolerance: +-10 mV) (unit: mV). */
-/* Temperature sensor */
-#define TEMPSENSOR_CAL1_ADDR               ((uint16_t*) (0x1FFF75A8U)) /* Internal temperature sensor, address of parameter TS_CAL1: On STM32L4, temperature sensor ADC raw data acquired at temperature  30 DegC (tolerance: +-5 DegC), Vref+ = 3.0 V (tolerance: +-10 mV). */
-#define TEMPSENSOR_CAL2_ADDR               ((uint16_t*) (0x1FFF75CAU)) /* Internal temperature sensor, address of parameter TS_CAL2: On STM32L4, temperature sensor ADC raw data acquired at temperature 110 DegC (tolerance: +-5 DegC), Vref+ = 3.0 V (tolerance: +-10 mV). */
-#define TEMPSENSOR_CAL1_TEMP               (( int32_t)   30)           /* Internal temperature sensor, temperature at which temperature sensor has been calibrated in production for data into TEMPSENSOR_CAL1_ADDR (tolerance: +-5 DegC) (unit: DegC). */
-#define TEMPSENSOR_CAL2_TEMP               (( int32_t)  110)           /* Internal temperature sensor, temperature at which temperature sensor has been calibrated in production for data into TEMPSENSOR_CAL2_ADDR (tolerance: +-5 DegC) (unit: DegC). */
-#define TEMPSENSOR_CAL_VREFANALOG          ( 3000U)                    /* Analog voltage reference (Vref+) voltage with which temperature sensor has been calibrated in production (+-10 mV) (unit: mV). */
-
-/* ADC registers bits positions */
-#define ADC_CFGR_RES_BITOFFSET_POS         ( 3U) /* Value equivalent to POSITION_VAL(ADC_CFGR_RES) */
-#define ADC_CFGR_AWD1SGL_BITOFFSET_POS     (22U) /* Value equivalent to POSITION_VAL(ADC_CFGR_AWD1SGL) */
-#define ADC_CFGR_AWD1EN_BITOFFSET_POS      (23U) /* Value equivalent to POSITION_VAL(ADC_CFGR_AWD1EN) */
-#define ADC_CFGR_JAWD1EN_BITOFFSET_POS     (24U) /* Value equivalent to POSITION_VAL(ADC_CFGR_JAWD1EN) */
-#define ADC_TR1_HT1_BITOFFSET_POS          (16U) /* Value equivalent to POSITION_VAL(ADC_TR1_HT1) */
-
-
-#define __LL_ADC_CONVERT_DATA_RESOLUTION(__DATA__,\
-                                         __ADC_RESOLUTION_CURRENT__,\
-                                         __ADC_RESOLUTION_TARGET__)            \
-  (((__DATA__)                                                                 \
-    << ((__ADC_RESOLUTION_CURRENT__) >> (ADC_CFGR_RES_BITOFFSET_POS - 1U)))    \
-   >> ((__ADC_RESOLUTION_TARGET__) >> (ADC_CFGR_RES_BITOFFSET_POS - 1U))       \
-  )
-
-
-#define __LL_ADC_CALC_TEMPERATURE(__VREFANALOG_VOLTAGE__,\
-                                  __TEMPSENSOR_ADC_DATA__,\
-                                  __ADC_RESOLUTION__)                              \
-  (((( ((int32_t)((__LL_ADC_CONVERT_DATA_RESOLUTION((__TEMPSENSOR_ADC_DATA__),     \
-                                                    (__ADC_RESOLUTION__),          \
-                                                    0x0)         \
-                   * (__VREFANALOG_VOLTAGE__))                                     \
-                  / TEMPSENSOR_CAL_VREFANALOG)                                     \
-        - (int32_t) *TEMPSENSOR_CAL1_ADDR)                                         \
-     ) * (int32_t)(TEMPSENSOR_CAL2_TEMP - TEMPSENSOR_CAL1_TEMP)                    \
-    ) / (int32_t)((int32_t)*TEMPSENSOR_CAL2_ADDR - (int32_t)*TEMPSENSOR_CAL1_ADDR) \
-   ) + TEMPSENSOR_CAL1_TEMP                                                        \
-  )
-
+} // namespace
 
 int main()
 {
     using namespace cml::common;
     using namespace cml::hal;
+    using namespace cml::utils;
 
     mcu::enable_hsi_clock(mcu::Hsi_frequency::_16_MHz);
     mcu::set_sysclk(mcu::Sysclk_source::hsi, { mcu::Bus_prescalers::AHB::_1,
@@ -82,53 +47,78 @@ int main()
     {
         mcu::set_nvic({ mcu::NVIC_config::Grouping::_4, 16u << 4u });
 
+        USART::Config usart_config =
+        {
+            115200u,
+            USART::Oversampling::_16,
+            USART::Word_length::_8_bits,
+            USART::Stop_bits::_1,
+            USART::Flow_control::none,
+            USART::Parity::none,
+        };
+
+        USART::Clock usart_clock
+        {
+            USART::Clock::Source::sysclk,
+            SystemCoreClock
+        };
+
+        Alternate_function_pin::Config usart_pin_config =
+        {
+            Alternate_function_pin::Mode::push_pull,
+            Alternate_function_pin::Pull::up,
+            Alternate_function_pin::Speed::low,
+            0x7u
+        };
+
         systick::enable(0x0);
 
-        ADC adc_1(ADC::Id::_1);
-
+        mcu::disable_msi_clock();
         mcu::enable_dwt();
+
+        ADC adc(ADC::Id::_1);
 
         GPIO gpio_c(GPIO::Id::c);
         gpio_c.enable();
 
-        bool ok = adc_1.enable(ADC::Resolution::_12_bit, { ADC::Synchronous_clock::Source::pclk,
-                                                           ADC::Synchronous_clock::Divider::_1 }, 20);
+        bool periph_ready = adc.enable(ADC::Resolution::_12_bit, { ADC::Synchronous_clock::Source::pclk,
+                                                                   ADC::Synchronous_clock::Divider::_1 },
+                                       20);
 
-        if (true == ok)
+        if (true == periph_ready)
         {
-            Analog_pin channel_2(&gpio_c, 1);
-            channel_2.enable(Input_pin::Pull::up);
+            GPIO gpio_port_a(GPIO::Id::a);
+            gpio_port_a.enable();
 
-            ADC::Channel channels[] = { ADC::Channel::Id::temperature_sensor, ADC::Channel::Sampling_time::_640_5_clock_cycles };
+            Alternate_function_pin console_usart_TX_pin(&gpio_port_a, 2);
+            Alternate_function_pin console_usart_RX_pin(&gpio_port_a, 3);
 
-            adc_1.set_active_channels(channels, 1);
+            console_usart_TX_pin.enable(usart_pin_config);
+            console_usart_RX_pin.enable(usart_pin_config);
 
-            uint16 buff = 0;
+            USART console_usart(USART::Id::_2);
+            bool preiph_ready = console_usart.enable(usart_config, usart_clock, 10);
 
-            while (true)
+            if (true == preiph_ready)
             {
-                wait = true;
-                adc_1.read_polling(&buff, 1);
-                //while (true == wait);
+                const ADC::Channel enabled_channels[] = { ADC::Channel::Id::temperature_sensor,
+                                                          ADC::Channel::Sampling_time::_640_5_clock_cycles };
 
-                auto c1 = adc_1.get_temperature_sensor_calibration_data_1().data;
-                auto c2 = adc_1.get_temperature_sensor_calibration_data_2().data;
+                adc.set_active_channels(enabled_channels, 1);
 
-                auto c11 = (int32_t)*TEMPSENSOR_CAL1_ADDR;
-                auto c22 = (int32_t)*TEMPSENSOR_CAL2_ADDR;
+                Console console(&console_usart);
+                console.enable();
 
-                //volatile auto temp = (100.0f / (c2 - c1)) * (d - c1) + 30.0f;
-                //temp = temp;
-                //d = d >> 4;
-                volatile auto temp = __LL_ADC_CALC_TEMPERATURE(3300, buff, 0x0);
-                temp = temp;
-                cml::utils::sleep::ms(10);
+                console.write_line("CML ADC sample. CPU speed: %d MHz", mcu::get_sysclk_frequency_hz() / MHz(1));
 
-                //ADC_CCR_TSEN
-                //uint16 data = 0;
-                //adc_1.read_it()
-                //adc_1.read_polling(&data, 1);
-                //data = data;
+                while (true)
+                {
+                    uint16 r = 0;
+                    adc.read_polling(&r, 1);
+
+                    console.write_line("temp: %d, adc: %u\r", compute_temperature(adc.get_calibration_data(), r), r);
+                    sleep::ms(10);
+                }
             }
         }
     }
