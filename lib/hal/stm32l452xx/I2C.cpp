7f15ad582947e9baeafbc94a38910a0ea3645651
msemegen <mateusz.semegen@gmail.com>
2020-04-05 23:09:05 +0200

    write_byte_polling for I2C_master

--- lib/hal/stm32l452xx/I2C.cpp
+++ lib/hal/stm32l452xx/I2C.cpp
@@ -8,6 +8,10 @@
 //this
 #include <hal/stm32l452xx/I2C.hpp>
 
+//cml
+#include <hal/core/systick.hpp>
+#include <utils/wait.hpp>
+
 namespace {
 
 using namespace cml::common;
@@ -67,10 +71,24 @@ void i2c_4_enable(uint32 a_clock_source, uint32 a_irq_priority)
     NVIC_EnableIRQ(I2C4_EV_IRQn);
 }
 
-void i2c_3_disable()
+void i2c_4_disable()
 {
     clear_flag(&(RCC->APB1ENR2), RCC_APB1ENR2_I2C4EN);
     NVIC_DisableIRQ(I2C4_EV_IRQn);
+}
+
+uint32 get_clock_source_register_value(uint32 a_clock_source, uint32 a_i2c_id)
+{
+    if (a_i2c_id < 4)
+    {
+        return a_clock_source << (12 + a_i2c_id * 2);
+    }
+    else
+    {
+        return a_clock_source;
+    }
+
+    return 0;
 }
 
 struct Controller
@@ -96,18 +114,78 @@ Controller controllers[]
 
 } // namespace ::
 
+extern "C"
+{
+
+void interupt_handler(uint32 a_controller_index)
+{
+    assert(nullptr != controllers[a_controller_index].p_i2c_master_handle ||
+           nullptr != controllers[a_controller_index].p_i2c_slave_handle);
+
+    if (nullptr != controllers[a_controller_index].p_i2c_master_handle)
+    {
+        i2c_handle_interrupt(controllers[a_controller_index].p_i2c_master_handle);
+    }
+
+    if (nullptr != controllers[a_controller_index].p_i2c_slave_handle)
+    {
+        i2c_handle_interrupt(controllers[a_controller_index].p_i2c_slave_handle);
+    }
+}
+
+void I2C1_EV_IRQHandler()
+{
+    interupt_handler(0);
+}
+
+void I2C2_EV_IRQHandler()
+{
+    interupt_handler(1);
+}
+
+void I2C3_EV_IRQHandler()
+{
+    interupt_handler(2);
+}
+
+void I2C4_EV_IRQHandler()
+{
+    interupt_handler(3);
+}
+
+} // extern "C"
+
 namespace cml {
 namespace hal {
 namespace stm32l452xx {
 
 using namespace cml::common;
+using namespace cml::hal::core;
+using namespace cml::utils;
 
-void I2C_master::enable(Clock_source a_clock_source, bool a_analog_filter, uint32 a_timings, bool a_is_fast_plus)
+void i2c_handle_interrupt(I2C_master* a_p_this)
+{
+
+}
+
+void i2c_handle_interrupt(I2C_slave* a_p_this)
+{
+
+}
+
+void I2C_master::enable(Clock_source a_clock_source,
+                        bool a_analog_filter,
+                        uint32 a_timings,
+                        bool a_is_fast_plus,
+                        uint32 a_irq_priority)
 {
     assert(nullptr == controllers[static_cast<uint32>(this->id)].p_i2c_master_handle);
     assert(nullptr == controllers[static_cast<uint32>(this->id)].p_i2c_slave_handle);
 
-    controllers[static_cast<uint32>(this->id)].enable();
+    controllers[static_cast<uint32>(this->id)].enable(get_clock_source_register_value(static_cast<uint32>(a_clock_source),
+                                                                                      static_cast<uint32>(this->id)),
+                                                      a_irq_priority);
+
     controllers[static_cast<uint32>(this->id)].p_i2c_master_handle = this;
     this->p_i2c = controllers[static_cast<uint32>(this->id)].p_registers;
 
@@ -138,37 +216,71 @@ void I2C_master::diasble()
     this->p_i2c = nullptr;
 }
 
-void I2C_master::write_bytes_polling(const void* a_p_data, uint32 a_data_size_in_bytes)
-{
-
-}
-
-bool I2C_master::write_bytes_polling(const void* a_p_data, uint32 a_data_size_in_bytes, time_tick a_timeout_ms)
-{
-    return false;
-}
-
-void I2C_master::read_bytes_polling(void* a_p_data, uint32 a_data_size_in_bytes)
+void I2C_master::write_bytes_polling(uint32 a_slave_address, const void* a_p_data, uint32 a_data_size_in_bytes)
 {
     assert(nullptr != controllers[static_cast<uint32>(this->id)].p_i2c_master_handle);
     assert(nullptr != a_p_data);
-    assert(a_data_size_in_bytes > 0);
+    assert(a_data_size_in_bytes > 0 && a_data_size_in_bytes <= 255);
 
-    clear_flag(&(this->p_i2c->CR2), I2C_CR2_NACK);
+    uint32 address_mask = (static_cast<uint32_t>(a_slave_address) << 1) & I2C_CR2_SADD;
+    uint32 data_length_mask = static_cast<uint32_t>(a_data_size_in_bytes) << I2C_CR2_NBYTES_Pos;
 
-    uint8* p_data = static_cast<uint8*>(a_p_data);
+    this->p_i2c->CR2 = address_mask | data_length_mask | I2C_CR2_AUTOEND | I2C_CR2_START;
+
     for (uint32 i = 0; i < a_data_size_in_bytes; i++)
     {
-        while (I2C_ISR_RXNE != READ_BIT(this->p_i2c->ISR, I2C_ISR_RXNE));
-        p_data[i] = static_cast<uint8_t>(this->p_i2c->RXDR);
+        wait::until(&(this->p_i2c->ISR), I2C_ISR_TXE, false);
+        this->p_i2c->TXDR = static_cast<const uint8*>(a_p_data)[i];
     }
+
+    set_flag(&(this->p_i2c->ICR), I2C_ICR_STOPCF);
+    this->p_i2c->CR2 = 0;
 }
 
-bool I2C_master::read_bytes_polling(void* a_p_data, uint32 a_data_size_in_bytes, time_tick a_timeout_ms)
+bool I2C_master::write_bytes_polling(uint32 a_slave_address,
+                                     const void* a_p_data,
+                                     uint32 a_data_size_in_bytes,
+                                     time_tick a_timeout_ms)
 {
-    return false;
+    assert(nullptr != controllers[static_cast<uint32>(this->id)].p_i2c_master_handle);
+    assert(nullptr != a_p_data);
+    assert(a_data_size_in_bytes > 0 && a_data_size_in_bytes <= 255);
+    assert(true == systick::is_enabled());
+
+    time_tick start = systick::get_counter();
+
+    uint32 address_mask = (static_cast<uint32_t>(a_slave_address) << 1) & I2C_CR2_SADD;
+    uint32 data_length_mask = static_cast<uint32_t>(a_data_size_in_bytes) << I2C_CR2_NBYTES_Pos;
+
+    this->p_i2c->CR2 = address_mask | data_length_mask | I2C_CR2_AUTOEND | I2C_CR2_START;
+
+    bool ret = true;
+    for (uint32 i = 0; i < a_data_size_in_bytes && true == ret; i++)
+    {
+        ret = wait::until(&(this->p_i2c->ISR), I2C_ISR_TXE, false, start, a_timeout_ms);
+
+        if (true == ret)
+        {
+            this->p_i2c->TXDR = static_cast<const uint8*>(a_p_data)[i];
+        }
+    }
+
+    set_flag(&(this->p_i2c->ICR), I2C_ICR_STOPCF);
+    this->p_i2c->CR2 = 0;
+
+    return ret;
 }
 
+void I2C_master::read_bytes_polling(uint32 a_slave_address, void* a_p_data, uint32 a_data_size_in_bytes)
+{
+}
+
+bool I2C_master::read_bytes_polling(uint32 a_slave_address,
+                                    void* a_p_data,
+                                    uint32 a_data_size_in_bytes,
+                                    time_tick a_timeout_ms)
+{
+}
 
 } // namespace stm32l452xx
 } // namespace hal
--- lib/hal/stm32l452xx/I2C.hpp
+++ lib/hal/stm32l452xx/I2C.hpp
@@ -35,9 +35,25 @@ public:
 
     enum class Clock_source
     {
-        pclk1,
-        sysclk,
-        hsi
+        pclk1  = 0,
+        sysclk = 1,
+        hsi    = 2
+    };
+
+    struct TX_callback
+    {
+        using Function = bool(*)(volatile common::uint8* p_a_data, void* a_p_user_data);
+
+        Function function = nullptr;
+        void* p_user_data = nullptr;
+    };
+
+    struct RX_callback
+    {
+        using Function = bool(*)(common::uint8 a_data, void* a_p_user_data);
+
+        Function function = nullptr;
+        void* p_user_data = nullptr;
     };
 
 public:
@@ -51,7 +67,11 @@ public:
         this->diasble();
     }
 
-    void enable(Clock_source a_clock_source, bool a_analog_filter, common::uint32 a_timings, bool a_is_fast_plus);
+    void enable(Clock_source a_clock_source,
+                bool a_analog_filter,
+                common::uint32 a_timings,
+                bool a_is_fast_plus,
+                common::uint32 a_irq_priority);
     void diasble();
 
     template<typename Data_t>
@@ -63,7 +83,7 @@ public:
     template<typename Data_t>
     bool write_polling(const Data_t& a_data, common::time_tick a_timeout_ms)
     {
-        return this->write_bytes_polling(static_cast<void*>(a_data), sizeof(a_data), a_timeout_ms);
+        return this->write_bytes_polling(static_cast<void*>(&a_data), sizeof(a_data), a_timeout_ms);
     }
 
     template<typename Data_t>
@@ -78,11 +98,29 @@ public:
         return this->write_bytes_polling(static_cast<void*>(&a_p_data), sizeof(Data_t), a_timeout_ms);
     }
 
-    void write_bytes_polling(const void* a_p_data, common::uint32 a_data_size_in_bytes);
-    bool write_bytes_polling(const void* a_p_data, common::uint32 a_data_size_in_bytes, common::time_tick a_timeout_ms);
+    void write_bytes_polling(common::uint32 a_slave_address,
+                             const void* a_p_data,
+                             common::uint32 a_data_size_in_bytes);
 
-    void read_bytes_polling(void* a_p_data, common::uint32 a_data_size_in_bytes);
-    bool read_bytes_polling(void* a_p_data, common::uint32 a_data_size_in_bytes, common::time_tick a_timeout_ms);
+    bool write_bytes_polling(common::uint32 a_slave_address,
+                             const void* a_p_data,
+                             common::uint32 a_data_size_in_bytes,
+                             common::time_tick a_timeout_ms);
+
+    void read_bytes_polling(common::uint32 a_slave_address,
+                            void* a_p_data,
+                            common::uint32 a_data_size_in_bytes);
+
+    bool read_bytes_polling(common::uint32 a_slave_address,
+                            void* a_p_data,
+                            common::uint32 a_data_size_in_bytes,
+                            common::time_tick a_timeout_ms);
+
+    void start_write_bytes_it(const TX_callback& a_callback);
+    void start_read_bytes_it(const RX_callback& a_callback);
+
+    void stop_write_bytes_it();
+    void stop_read_bytes_it();
 
     bool is_analog_filter() const
     {
@@ -117,6 +155,10 @@ private:
 
     Id id;
     I2C_TypeDef* p_i2c;
+
+private:
+
+    friend void i2c_handle_interrupt(I2C_master* a_p_this);
 };
 
 class I2C_slave : private common::Non_copyable
@@ -203,6 +245,11 @@ private:
 
     Id id;
     I2C_TypeDef* p_i2c;
+
+private:
+
+    friend void i2c_handle_interrupt(I2C_slave* a_p_this);
+
 };
 
 } // namespace stm32l452xx
