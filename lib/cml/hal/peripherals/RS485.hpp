60fe49573a3812b3b301629cae408dfea2d8136a
msemegen <mateusz.semegen@gmail.com>
2020-07-10 17:52:23 +0200

    Added missed file comment

--- lib/cml/hal/peripherals/RS485.hpp
+++ lib/cml/hal/peripherals/RS485.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
+/*
+    Name: RS485.hpp
+
+    Copyright(c) 2020 Mateusz Semegen
+    This code is licensed under MIT license (see LICENSE file for details)
+*/
+
 #ifdef STM32L452xx
 #include <soc/stm32l452xx/peripherals/RS485.hpp>
 #endif // STM32L452xx
